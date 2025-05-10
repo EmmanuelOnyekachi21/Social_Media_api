a4bfc6681d27fada24e990fdc9dd68811fad9147
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-05-10 04:43:50 +0100

    Created the User model

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'core.user'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core_user.User'
