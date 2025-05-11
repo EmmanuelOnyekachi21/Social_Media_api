c714332706ab07372c20c7af3f063bbdc0f364eb
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-05-11 07:25:56 +0100

    Wrote the Post serializer and viewset

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewset
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
+from core.post.viewsets import PostViewSet
 
 router = routers.SimpleRouter()
 
@@ -11,6 +12,9 @@ router.register(r'user', UserViewset, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+# Post ROuter
+router.register(r'post', PostViewSet, basename='post')
 
 urlpatterns = [
     # *router.urls is unpacking the list of URL patterns.
